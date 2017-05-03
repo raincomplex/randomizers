308d4847ff800c2e1995f73d0c64f63aa1d55ca6
Paul Becker <raincomplex@gmail.com>
2017-05-03 17:17:24 -0400

    move randomizer loading into a module

---
+++ load.py
@@ -0,0 +1,25 @@
+import re, glob, importlib
+
+modulenames = glob.glob('randos/*.py')
+modulenames = [re.sub(r'randos/(.*)\.py$', lambda m: m.group(1), n) for n in modulenames]
+modulenames.remove('__init__')
+
+modules = {}
+for m in modulenames:
+    modules[m] = importlib.import_module('randos.' + m)
+
+rands = {}
+for name, m in sorted(modules.items()):
+    if hasattr(m, 'factory'):
+        rands[name] = m.factory
+    elif hasattr(m, 'factory1'):
+        i = 1
+        while hasattr(m, 'factory' + str(i)):
+            f = getattr(m, 'factory' + str(i))
+            n = name + '/' + (f.__doc__ or str(i))
+            rands[n] = f
+            i += 1
+    elif hasattr(m, 'Randomizer'):
+        rands[name] = m.Randomizer
+    else:
+        print 'module has no factory functions or Randomizer class:', name
--- main.py
+++ main.py
@@ -1,39 +1,16 @@
 #!/usr/bin/python2
-import importlib, re, sys, glob
+import re, sys
 from multiprocessing import Pool, cpu_count
 
+import load
 from analysis import analyze
 import compare
-
-modulenames = glob.glob('randos/*.py')
-modulenames = [re.sub(r'randos/(.*)\.py$', lambda m: m.group(1), n) for n in modulenames]
-modulenames.remove('__init__')
 
 count = 100000
 if '--fast' in sys.argv:
     count = 10000
 
 # end config
-
-modules = {}
-for m in modulenames:
-    modules[m] = importlib.import_module('randos.' + m)
-
-rands = []
-for name, m in sorted(modules.items()):
-    if hasattr(m, 'factory'):
-        rands.append((name, m.factory))
-    elif hasattr(m, 'factory1'):
-        i = 1
-        while hasattr(m, 'factory' + str(i)):
-            f = getattr(m, 'factory' + str(i))
-            n = name + '/' + (f.__doc__ or str(i))
-            rands.append((n, f))
-            i += 1
-    elif hasattr(m, 'Randomizer'):
-        rands.append((name, m.Randomizer))
-    else:
-        print 'module has no factory functions or Randomizer class:', name
 
 def process(t):
     name, factory = t
@@ -46,7 +23,7 @@ def process(t):
     return (name, a)
 
 pool = Pool(cpu_count())
-m = pool.map(process, rands)
+m = pool.map(process, load.rands.items())
 print
 for t in m:
     name, a = t
