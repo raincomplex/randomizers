db75f403066cbdc53f59ec3b179555a2c49a60bf
Paul Becker <raincomplex@gmail.com>
2017-05-04 00:55:10 -0400

    move randomizer tests into unittest framework

--- randos/tap.py
+++ randos/tap.py
@@ -1,3 +1,4 @@
+import unittest
 import tgm
 
 class Randomizer(tgm.Randomizer):
@@ -5,16 +6,11 @@ class Randomizer(tgm.Randomizer):
         tgm.Randomizer.__init__(self, seed=seed, rolls=6)
         self.history = [self.history[0], 1, 2, 2]
 
-def test():
-    r = Randomizer(tgm.unrand(0xeb48c724))
-    goal = 'tijostzijslzijsltozjsltojzltsoizljsizlto'
-    s = ''
-    for i in range(len(goal)):
-        s += r.next()
-
-    print goal
-    print s
-    assert s == goal
-
-if __name__ == '__main__':
-    test()
+class Tests(unittest.TestCase):
+    def testPRNG(self):
+        r = Randomizer(tgm.unrand(0xeb48c724))
+        goal = 'tijostzijslzijsltozjsltojzltsoizljsizlto'
+        s = ''
+        for i in range(len(goal)):
+            s += r.next()
+        assert s == goal
