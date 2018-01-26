11d2d66cc3b06d3b48cefe969b15aada538356e4
Paul Becker <raincomplex@gmail.com>
2018-01-26 10:00:51 -0500

    add another weight-based randomizer

---
+++ randos/weight2.py
@@ -0,0 +1,19 @@
+import random
+
+class Randomizer:
+    def __init__(self):
+        self.weights = [1] * 7
+    
+    def next(self):
+        for i in range(7):
+            if self.weights[i] > 1:
+                self.weights[i] -= 1
+
+        total = sum(1. / w for w in self.weights)
+        n = random.random() * total
+
+        for i, w in enumerate(self.weights):
+            n -= 1. / w
+            if n < 0 or i == 6:
+                self.weights[i] += 5
+                return 'jiltsoz'[i]
