9e3c7e56ec8a6dcf23c6fa53fcab09a4a00b00d9
Paul Becker <raincomplex@gmail.com>
2019-07-14 17:43:43 -0400

    script for sampling randomizers

---
+++ sample.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+import sys, load
+
+count = 1000
+
+names = sys.argv[1:]
+if not names:
+    names = sorted(load.rands.keys())
+
+for name in names:
+    rand = load.rands[name]()
+    s = ''
+    for i in range(count):
+        s += rand.next()
+
+    print name
+    print s
+    print
