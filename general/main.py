ba3d079c6ad40100c0e764aaf5b7f99fcf0c57f7
Paul Becker <raincomplex@gmail.com>
2019-07-14 17:04:28 -0400

    add generalized randomizer

---
+++ general/main.py
@@ -0,0 +1,82 @@
+import random
+import randos
+
+def run(randfunc, count=1000):
+    'generate a piece sequence with the given randomizer function'
+    history = []
+    while len(history) < count:
+        r = randfunc(history)
+        p = generatePiece(r)
+        history.append(p)
+    return history
+
+def runprob(randfunc, count, history=None):
+    'find the probability of the next piece after "count" pieces, considering all possiblilities'
+    if history is None:
+        history = []
+
+    r = randfunc(history)
+    if count == 0:
+        total = sum(r.values())
+        return {c: v / total for c, v in r.items()}
+    
+    s = {}
+    for c in 'jiltsoz':
+        w = r.get(c, 0)
+        if w > 0:
+            x = runprob(randfunc, count - 1, history + [c])
+            for d in 'jiltsoz':
+                s[d] = s.get(d, 0) + w * x.get(d, 0)
+
+    total = sum(s.values())
+    return {c: v / total for c, v in s.items()}
+
+def generatePiece(probs):
+    '''
+    generate a random piece from the weights given:
+        probs = {"j": <float>, "i": <float>, ...}.
+    
+    missing weights are taken to be 0.
+    at least one non-zero weight must be given.
+    '''
+    total = sum(probs.values())
+    if total <= 0:
+        raise ValueError('sum of weights must be > 0')
+    r = random.random()
+    for c in 'jiltso':
+        r -= probs.get(c, 0) / total
+        if r < 0:
+            return c
+    return 'z'
+
+def formatProbs(p):
+    return '{%s}' % ', '.join('%s %.3f' % (c, p.get(c, 0)) for c in 'jiltsoz')
+
+if __name__ == '__main__':
+    import sys
+    
+    count = 10000
+    display = 80
+    showrunprob = ('-p' in sys.argv)
+    
+    funclist = []
+    for name in dir(randos):
+        if not name.startswith('_'):
+            funclist.append(getattr(randos, name))
+    funclist.sort(key=lambda f: f.__code__.co_firstlineno)
+    
+    for func in funclist:
+        print(func.__name__)
+        seq = ''.join(run(func, count))
+        print(seq[:display])
+        
+        doubles = 0
+        for i in range(len(seq) - 1):
+            if seq[i] == seq[i+1]:
+                doubles += 1
+        print('doubles', doubles / count)
+        if showrunprob:
+            for c in range(7):
+                print(c, formatProbs(runprob(func, c)))
+
+        print()
---
+++ general/randos.py
@@ -0,0 +1,79 @@
+
+def pure(history):
+    'equal probability regardless of history'
+    return {'j': 1, 'i': 1, 'l': 1, 't': 1, 's': 1, 'o': 1, 'z': 1}
+
+def flat(history):
+    'fixed sequence'
+    return {'jiltsoz'[len(history) % 7]: 1}
+
+
+def bag(history):
+    'standard 7-piece bag'
+    used = len(history) % 7
+    bag = set('jiltsoz')
+    if used > 0:
+        bag -= set(history[-used:])
+    return {c: 1 for c in bag}
+
+
+def _recent(history, op):
+    p = {}
+    w = 1
+    for c in reversed(history):
+        if c not in p:
+            p[c] = w
+            w = op(w)
+            if len(p) == 7:
+                break
+    for c in 'jiltsoz':
+        if c not in p:
+            p[c] = w
+    return p
+
+def exp(history):
+    'weighted according to recent-ness, per piece type, multiplicatively.'
+    return _recent(history, lambda w: w*2)
+
+def lin(history):
+    'weighted according to recent-ness, per piece type, additively.'
+    return _recent(history, lambda w: w+1)
+
+
+def nes(history):
+    if len(history) > 0:
+        last = history[-1]
+        # 7 points to each of 'jiltsozx'
+        # last and x get rerolled into 'jiltsoz' (14/7 = +2 each)
+        p = {c: (9 if c != last else 2) for c in 'jiltsoz'}
+    else:
+        # 7 points to each of 'jiltsozx'
+        # x gets rerolled into 'jiltsoz' (+1 each)
+        p = {c: 8 for c in 'jiltsoz'}
+    return p
+
+def tgm(history, rolls=4, start='zzzz'):
+    if len(history) == 0:
+        return {'j': 1, 'i': 1, 'l': 1, 't': 1}
+    
+    history = history[-4:]
+    if len(history) < 4:
+        history = (list(start) + history)[-4:]
+
+    p = {c: 0 for c in 'jiltsoz'}
+    w = 1
+    for i in range(rolls - 1):
+        carry = 0
+        for c in 'jiltsoz':
+            if c in history:
+                carry += w
+            else:
+                p[c] += w
+        w = carry / 7
+    for c in 'jiltsoz':
+        p[c] += w
+
+    return p
+
+def tap(history):
+    return tgm(history, rolls=6, start='szsz')
