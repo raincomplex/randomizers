467319bc4297f9501c4d79c94948de6f3c1dbac4
Paul Becker <raincomplex@gmail.com>
2019-07-14 21:18:34 -0400

    add drought protection flag to "exp"

--- randos/exp.py
+++ randos/exp.py
@@ -3,12 +3,31 @@ import random
 class Randomizer:
     def __init__(self):
         self.history = list('ojltisz')
+        self.actual = []
         self.chance = .5
+        self.protect = True  # drought protection
 
     def next(self):
-        for i in range(len(self.history) - 1):
-            if random.random() < self.chance:
-                x = self.history.pop(i)
-                self.history.append(x)
-                return x
-        return self.history[-1]
+        x = None
+
+        if self.protect:
+            for p in 'jiltsoz':
+                if len(self.actual) == 12 and p not in self.actual:
+                    x = p
+                    break
+
+        if x is None:
+            for i in range(len(self.history) - 1):
+                if random.random() < self.chance:
+                    x = self.history.pop(i)
+                    self.history.append(x)
+                    break
+
+        if x is None:
+            x = self.history[-1]
+
+        if len(self.actual) == 12:
+            self.actual.pop(0)
+        self.actual.append(x)
+
+        return x
