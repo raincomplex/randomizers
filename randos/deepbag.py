3475d74ffdde111d6a904e3738fe47c4488296ab
Paul Becker <raincomplex@gmail.com>
2019-07-17 12:56:32 -0400

    merge foreverbag into deepbag

--- randos/deepbag.py
+++ randos/deepbag.py
@@ -1,18 +1,54 @@
+'pieces are drawn from a bag (the window) which is refilled by a second bag'
 import random
 
-class deepbag:
-    def __init__(self):
-        self.bag = list('jiltsoz')
-        self.rebag()
+def deepbag_window4():
+    'window=4'
+    return DeepBag(windowsize=4)
+
+def deepbag_window7():
+    'window=7'
+    return DeepBag(windowsize=7)
+
+def deepbag_window10():
+    'window=10'
+    return DeepBag(windowsize=10)
+
+def deepbag_fixed4():
+    'window=4, fixed second bag'
+    return DeepBag(windowsize=4, shuffle=False)
+
+def deepbag_fixed7():
+    'window=7, fixed second bag'
+    return DeepBag(windowsize=7, shuffle=False)
+
+def deepbag_fixed10():
+    'window=10, fixed second bag'
+    return DeepBag(windowsize=10, shuffle=False)
+
+class DeepBag:
+    def __init__(self, bag='jiltsoz', windowsize=7, shuffle=True):
+        self.window = []
+        self.size = windowsize
         
-    def rebag(self):
-        self.bag2 = list('jiltsoz')
-        random.shuffle(self.bag2)
+        self.bag = list(bag)
+        self.shuffle = shuffle
+        self.i = 0
+
+        if self.shuffle:
+            random.shuffle(self.bag)
         
+        for i in range(self.size):
+            self.window.append(self.bag[self.i])
+            self.advance()
+
     def next(self):
-        i = random.randint(0, len(self.bag) - 1)
-        p = self.bag[i]
-        self.bag[i] = self.bag2.pop()
-        if len(self.bag2) == 0:
-            self.rebag()
+        r = random.randint(0, self.size - 1)
+        p = self.window[r]
+        self.window[r] = self.bag[self.i]
+        self.advance()
         return p
+
+    def advance(self):
+        self.i = (self.i + 1) % len(self.bag)
+        if self.i == 0 and self.shuffle:
+            random.shuffle(self.bag)
