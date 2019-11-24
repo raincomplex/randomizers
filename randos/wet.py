60a7e402c37419e2112511185a662e2ee5ff4ab5
Paul Becker <raincomplex@gmail.com>
2019-11-24 12:00:24 -0500

    add wet variation with large history

--- randos/wet.py
+++ randos/wet.py
@@ -23,8 +23,8 @@ class wet:
         return p
 
 class wet2:
-    def __init__(self):
-        self.size = 12
+    def __init__(self, size=12):
+        self.size = size
         self.history = list('jiltsoz')
 
     def next(self):
@@ -52,6 +52,40 @@ class wet2:
         self.history.append(p)
 
         return p
+
+class wet3:
+    'identical to wet2 for low history sizes (up to 14?), but unlike wet2 it will still balance counts on larger histories'
+    def __init__(self, size=100):
+        self.size = size
+        self.history = []
+
+    def next(self):
+        d = {}
+        for c in 'jiltsoz':
+            d[c] = self.history.count(c)
+
+        for i in range(0, self.size + 1):
+            lst = []
+            for k, c in d.items():
+                if c == i:
+                    lst.append(k)
+            if lst:
+                p = random.choice(lst)
+                break
+
+        if len(self.history) == self.size:
+            self.history.pop(0)
+        self.history.append(p)
+
+        return p
+
+def wet2_size100():
+    'degrades to random'
+    return wet2(size=100)
+
+def wet3_size12():
+    'identical to wet2'
+    return wet3(size=12)
 
 def wet_pure(history):
     size = 12
